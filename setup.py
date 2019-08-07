7dce241688e00bbed8e0e2694276b8c32cb7388e
Matthew D. Scholefield <matthew331199@gmail.com>
2019-08-06 21:58:25 -0700

    Bump version to 0.1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ package_data = [
 
 setup(
     name='proyo',
-    version='0.1.2',
+    version='0.1.4',
     description='A tool to broadcast notifications across various interfaces',
     url='https://github.com/matthewscholefield/proyo',
     author='Matthew Scholefield',
