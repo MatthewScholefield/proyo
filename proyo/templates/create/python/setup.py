a7717772fa6c6dcda41bd972881e440907071ea6
Matthew D. Scholefield <matthew331199@gmail.com>
2022-08-06 18:29:57 -0700

    Update python version tags in setup.py

--- proyo/templates/create/python/setup.py
+++ proyo/templates/create/python/setup.py
@@ -15,9 +15,9 @@ setup(
 
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     keywords='{{project_name.replace("-", " ")}}',
     # ~ if is_package:
