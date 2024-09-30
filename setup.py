37aa5d4f1787fea0ce6a1df1ffb1ada9c36b4a5e
Matthew D. Scholefield <matthew331199@gmail.com>
2024-09-29 22:58:08 -0700

    Fix last remaining hardcoded reference in pyproject.toml
    oops

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ package_data = [
 
 setup(
     name="proyo",
-    version="0.2.2",
+    version="0.2.3",
     description="A tool to broadcast notifications across various interfaces",
     url="https://github.com/matthewscholefield/proyo",
     author="Matthew Scholefield",
